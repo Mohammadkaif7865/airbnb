06ed2dd8fe593c4a7170004030793e2806c68515
Mohammadkaif <mohammadkaif051197@gmail.com>
2022-06-13 11:07:33 +0530

    yes me

--- index.js
+++ index.js
@@ -33,12 +33,12 @@ app.get('/amenities', (req, res) => {
         db.collection('listingsAndReviews').find({ $and: [{ "amenities": { $in: [req.query.spec1] } }, { "amenities": { $in: [req.query.spec2] } }, { "amenities": { $in: [req.query.spec3] } }, { "amenities": { $in: [req.query.spec4] } }] }).toArray((err, result) => {
             if (err) throw err;
             res.send(result);
-        })
+        });
     } else if (req.query.spec1 && req.query.spec2 && req.query.spec3) {
         db.collection('listingsAndReviews').find({ $and: [{ "amenities": { $in: [req.query.spec1] } }, { "amenities": { $in: [req.query.spec2] } }, { "amenities": { $in: [req.query.spec3] } }] }).toArray((err, result) => {
             if (err) throw err;
             res.send(result);
-        })
+        });
     } else if (req.query.spec1 && req.query.spec2) {
         db.collection('listingsAndReviews').find({ $and: [{ "amenities": { $in: [req.query.spec1] } }, { "amenities": { $in: [req.query.spec2] } }] }).toArray((err, result) => {
             if (err) throw err;
@@ -48,21 +48,27 @@ app.get('/amenities', (req, res) => {
         db.collection('listingsAndReviews').find({ $and: [{ "amenities": { $in: [req.query.spec1] } }] }).toArray((err, result) => {
             if (err) throw err;
             res.send(result);
-        })
+        });
     }
 });
 app.get('/beds', (req, res) => {
     db.collection('listingsAndReviews').find({ "beds": { $gt: 15 } }).sort({ "price": 1 }).toArray((err, result) => {
         if (err) throw err;
         res.send(result);
-    })
-})
+    });
+});
 app.get('/region/:country', (req, res) => {
     db.collection('listingsAndReviews').find({ "address.country": req.params.country }).sort({ "price": 1 }).toArray((err, result) => {
         if (err) throw err;
         res.send(result);
-    })
-})
+    });
+});
+app.get('/region/:country/sort', (req, res) => {
+    db.collection('listingsAndReviews').find({ $and: [{ "address.country": req.params.country }, { "price": { $gt: req.query.lCost, $lt: req.query.hCost } }] }).sort({ "price": 1 }).toArray((err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 MongoClient.connect(mongoUrl, (err, client) => {
     if (err) {
         console.error(`Error while connecting to MongoDB: ${err}`);
@@ -71,6 +77,6 @@ MongoClient.connect(mongoUrl, (err, client) => {
     app.listen(port, (err) => {
         if (err) throw err;
         console.log(`Express Server listening on port ${port}`);
-    })
+    });
 });
 // {$and:[{"address.country":{$ne:"Turkey"}},{"address.country":{$ne:"Australia"}},{"address.country":{$ne:"United States"}},{"address.country":{$ne:"Portugal"}},{"address.country":{$ne:"Spain"}},{"address.country":{$ne:"Hong Kong"}},{"address.country":{$ne:"Canada"}},{"address.country":{$ne:"Brazil"}}]}
